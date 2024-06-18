463da2c01650affc5a2d73c36c0c9a1f52dea800
PremkumarGM <gmprem007@gmail.com>
2024-06-18 20:44:51 +0530

    Update CSVtoXMLTradeConverter.cpp

--- CSVtoXMLTradeConverter.cpp
+++ CSVtoXMLTradeConverter.cpp
@@ -8,7 +8,7 @@
 #define MAX_TRADE_RECORDS 1024
 #define MAX_LINE_LENGTH 1024
 
-readTradeDataFromCsv(FILE* stream)
+void readTradeDataFromCsv(FILE* stream)
 {
 while (fgets(line, sizeof(line), stream)) {
         
@@ -19,15 +19,9 @@ while (fgets(line, sizeof(line), stream)) {
             token = strtok(NULL, ",");
         }
 }
-
-void ConvertDatafromCsvtoXML(FILE* stream) {
-    char line[MAX_LINE_LENGTH];
-    Trade_Record Records[MAX_TRADE_RECORDS];
-    int lineCount = 0;
-    int currentRecord = 0;
-    char* fields[3];
-    readTradeDataFromCsv(stream);
-    
+}
+void validateTradeData(void)
+{
         if (fieldCount != 3) {
             fprintf(stderr, "WARN: Line %d malformed. Only %d field(s) found.\n", lineCount + 1, fieldCount);
             continue;
@@ -47,16 +41,19 @@ void ConvertDatafromCsvtoXML(FILE* stream) {
         if (!TrytoConvertintoDouble(fields[2], &tradeprice)) {
             fprintf(stderr, "WARN: Trade price on line %d not a valid decimal: '%s'\n", lineCount + 1, fields[2]);
         }
-
+}
+void   mapCsvLineDataToTradeData(void)
+{
         strncpy(Records[currentRecord].Source_Currency, fields[0], 3);
         strncpy(Records[currentRecord].Destibation_Currency, fields[0] + 3, 3);
         Records[currentRecord].Lots = tradeamount / LotSize;
         Records[currentRecord].Price = tradeprice;
         currentRecord++;
         lineCount++;
-    }
-
-    FILE* outFile = fopen("outradepriceut.xml", "w");
+}
+void writeTradeDataToXML(void)
+{
+ FILE* outFile = fopen("outradepriceut.xml", "w");
     fprintf(outFile, "<TradeRecords>\n");
     for (int TradeRecord_index = 0; TradeRecord_index < currentRecord; TradeRecord_index++) {
         fprintf(outFile, "\t<TradeRecord>\n");
@@ -68,6 +65,18 @@ void ConvertDatafromCsvtoXML(FILE* stream) {
     }
     fprintf(outFile, "</TradeRecords>");
     fclose(outFile);
-    printf("INFO: %d trades ConvertDatafromCsvtoXMLed\n", currentRecord);
+    printf("INFO: %d trades ConvertDatafromCsvtoXMLed\n", currentRecord);       
+}
+void ConvertDatafromCsvtoXML(FILE* stream) {
+    char line[MAX_LINE_LENGTH];
+    Trade_Record Records[MAX_TRADE_RECORDS];
+    int lineCount = 0;
+    int currentRecord = 0;
+    char* fields[3];
+    readTradeDataFromCsv(stream);
+    validateTradeData();
+    mapCsvLineDataToTradeData();
+    writeTradeDataToXML();
+           
 }
 
